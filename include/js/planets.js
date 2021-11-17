cdc28e895b9c43ba92d8a1409f641144ed87a649
StanGlazenburg <48719495+StanGlazenburg@users.noreply.github.com>
2021-11-17 14:39:38 +0100

    Sort

---
+++ include/js/planets.js
@@ -0,0 +1,11 @@
+class planets{
+    constructor(name, pos, positionSpeed, rotationSpeed, size, texture, disSun) {
+        this.name = name;
+        this.pos = pos;
+        this.positionSpeed = positionSpeed;
+        this.rotationSpeed = rotationSpeed;
+        this.size = size;
+        this.texture = texture;
+        this.disSun = disSun;
+      }   
+}
